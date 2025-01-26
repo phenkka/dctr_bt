b24b3ddc7628a734d3bb6a7aeed5e7673d9790a9
phenkka <phenkka21@gmail.com>
2025-01-26 12:36:13 +0500

    add router and first command

---
+++ app/handlers.py
@@ -0,0 +1,9 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+
+router = Router()
+
+@router.message(Command("start"))
+async def cmd_start(message: Message):
+    await message.answer("Здесь подробная информация о боте")
--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print('Hello, world!')
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+
+import app.config as cfg
+from app.handlers import router
+
+bot = Bot(token=cfg.BOT_API)
+dp = Dispatcher()
+
+async def main():
+    dp.include_router(router)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Exit")
