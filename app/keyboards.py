ce525bb85acd7d008cc21e92a760629931266d17
phenkka <phenkka21@gmail.com>
2025-01-26 21:16:16 +0500

    adding more keyboards, like 'yes or nor' or 'gender'

--- app/keyboards.py
+++ app/keyboards.py
@@ -3,3 +3,13 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 greeting = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text="Take the survey", callback_data="survey")]
 ])
+
+gender = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Male", callback_data="male"),
+     InlineKeyboardButton(text='Female', callback_data="female")]
+])
+
+yon = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Yes", callback_data="yes"),
+     InlineKeyboardButton(text="No", callback_data="no")]
+])
