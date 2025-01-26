f77a2f72650575495f10c12b5a36c87056a45cff
phenkka <phenkka21@gmail.com>
2025-01-26 13:00:31 +0500

    adding a logger and writing logs to a separate file "bot.log"

--- app/handlers.py
+++ app/handlers.py
@@ -2,8 +2,11 @@ from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
 
+from logs.logging_config import logger
+
 router = Router()
 
 @router.message(Command("start"))
 async def cmd_start(message: Message):
+    logger.info(f"The user {message.from_user.id} called the command /start")
     await message.answer("Здесь подробная информация о боте")
--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from aiogram import Bot, Dispatcher
 
 import app.config as cfg
 from app.handlers import router
+from logs.logging_config import logger
 
 bot = Bot(token=cfg.BOT_API)
 dp = Dispatcher()
 
 async def main():
+    logger.info("Bot is started. Polling begins")
     dp.include_router(router)
     await dp.start_polling(bot)
 
@@ -18,4 +20,5 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
+        logger.warning("Bot is stopped manually")
         print("Exit")
